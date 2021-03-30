fe2dc5ac1ad80b45bb919e385536faa2cf234871
Alberto <edhoru@hotmail.com>
2021-03-29 23:12:30 -0600

    Initial Commit

---
+++ Sources/Colorful/Colorful.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+public extension UIColor {
+    
+    static var greeny: UIColor {
+        return #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+    }
+    
+}
+
